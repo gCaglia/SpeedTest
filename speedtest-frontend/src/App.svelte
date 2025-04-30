1dd9da90cae351feed420f59e98112cdf55da413
Giulia Caglia <cagliagiulia42@gmail.com>
2025-04-30 06:12:37 +0200

    Refactor (frontend): Clean up css and HTML

--- speedtest-frontend/src/App.svelte
+++ speedtest-frontend/src/App.svelte
@@ -1,43 +1,59 @@
 <script lang="ts">
   import { SpeedTest } from "./components/SpeedTest";
   import { Api } from "./components/api";
+  import "./components/SpeedSection.svelte"
+    import SpeedSection from "./components/SpeedSection.svelte";
 
   let api = new Api("http://localhost:3000");
   let speedTest = new SpeedTest(api);
 
-  let ping: number | null = null;
-  let downloadSpeed: number | null = null;
-  let uploadSpeed: number | null = null;
+  let ping: number[] | null = null;
+  let downloadSpeed: number[] | null = null;
+  let uploadSpeed: number[] | null = null;
 
   async function getPing() {
-    ping = await speedTest.ping();
+    ping = [];
+    for (let i = 0; i < 30; i++) {
+      ping.push(await speedTest.ping());
+    }
   }
 
   async function getDownloadSpeed() {
-    downloadSpeed = await speedTest.download();
+    downloadSpeed = [];
+    for (let i = 0; i < 30; i++) {
+      downloadSpeed.push(await speedTest.download());
+    }
   }
 
   async function getUploadSpeed() {
-    uploadSpeed = await speedTest.upload()
+    uploadSpeed = [];
+    for (let i = 0; i < 30; i++) {
+      uploadSpeed.push(await speedTest.upload());
+    }
   }
+
+  async function runTests() {
+    await getPing();
+    await getDownloadSpeed();
+    await getUploadSpeed();
+  }
+
 </script>
 
 <main>
-  <h1>Welcome to the Private SpeedTest!</h1>
-  <button on:click={getPing}>Test Ping </button>
-  {#if ping !== null}
-    <p>Your ping is: {ping.toFixed(2)} ms</p>
-  {/if}
-  <button on:click={getDownloadSpeed}>Test Download Speed</button>
-  {#if downloadSpeed !== null}
-    <p>Your download speed is: {downloadSpeed.toFixed(2)} MB/s.</p>
-  {/if}
-  <button on:click={getUploadSpeed}>Test Upload Speed</button>
-  {#if uploadSpeed !== null}
-    <p>Your upload speed is: {uploadSpeed.toFixed(2)} MB/s.</p>
-  {/if}
+  <div class="heading-box">
+    <h1> Welcome to the Private SpeedTest!</h1>
+  </div>
+  <SpeedSection ></SpeedSection>
+  <SpeedSection ></SpeedSection>
 </main>
 
 <style lang="postcss">
   @reference "tailwindcss";
+  .heading-box {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%
+  }
 </style>
