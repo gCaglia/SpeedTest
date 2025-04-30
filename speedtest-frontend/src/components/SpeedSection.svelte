930ab863c59e925502bdb22f10029b2cac8b4d2a
Giulia Caglia <cagliagiulia42@gmail.com>
2025-04-30 06:13:06 +0200

    WIP SpeedSection

---
+++ speedtest-frontend/src/components/SpeedSection.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    async function getStats() {
+        let results = {
+            "last": 1.1,
+            "avg": 5.1,
+            "max": 3.1
+        }
+        return results
+    }
+    let statsPromise = getStats();
+
+</script>
+
+<style lang="postcss">
+    .speed-section {
+        display: flex;
+        gap: 2rem
+        margin-bottom: 2rem;
+        align-items: center;
+        margin-top: 5rem
+    }
+
+</style>
+
+<main>
+    <div class="speed-section">
+        <div class="graph">
+            Download Graph (Placeholder)
+        </div>
+        <div class="stats">
+            {#await statsPromise}
+            <p>Loading Stats...</p>
+            {:then stats}
+            {#each Object.entries(stats) as [key, value]}
+            <div class="stat-box">
+                {key}: {value}
+            </div>
+            {/each}
+            {/await}
+        </div>
+    </div>
+
+</main>
